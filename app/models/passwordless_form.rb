d081049ea4fffbdc687ed10bdb2114ddfcab5948
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2025-06-23 17:01:59 +0100

    Update email regex

--- app/models/passwordless_form.rb
+++ app/models/passwordless_form.rb
@@ -2,6 +2,8 @@ class PasswordlessForm
   include ActiveModel::Model
   attr_accessor :email
 
+  EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\.[^@\s]+\z/
+
   validates :email, presence: { message: "Enter an email address in the correct format, like name@example.com" },
-                    format: { with: URI::MailTo::EMAIL_REGEXP, message: "Enter an email address in the correct format, like name@example.com" }
+                    format: { with: EMAIL_REGEX, message: "Enter an email address in the correct format, like name@example.com" }
 end
