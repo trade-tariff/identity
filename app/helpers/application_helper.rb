59ec00d4e5d0e9e000607ac26e287904f08c67fe
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2025-06-30 14:36:54 +0100

    Add page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def page_title(title = nil, form_object = nil)
+    default = "UK Online Trade Tariff"
+    base_title = "#{title} | #{default}"
+
+    if form_object&.errors&.any?
+      base_title = "Error: #{base_title}"
+    end
+
+    if title
+      content_for :title, base_title
+    else
+      content_for(:title) || default
+    end
+  end
 end
