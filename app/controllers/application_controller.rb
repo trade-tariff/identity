7ba27696efec3252da29117fc66d1d6c617d50a4
William Fish <william.michael.fish@gmail.com>
2026-01-19 16:33:43 +0000

    BAU: Environment conditional cookie names

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,21 @@ class ApplicationController < ActionController::Base
   end
 
   def clear_cookies
-    cookies.delete(:id_token, domain: ".#{current_consumer.cookie_domain}")
-    cookies.delete(:refresh_token, domain: ".#{current_consumer.cookie_domain}")
+    cookies.delete(id_token_cookie_name, domain: ".#{current_consumer.cookie_domain}")
+    cookies.delete(refresh_token_cookie_name, domain: ".#{current_consumer.cookie_domain}")
   end
 
 private
 
   def consumer_id
     params[:consumer_id] || session[:consumer_id]
+  end
+
+  def id_token_cookie_name
+    TradeTariffIdentity.id_token_cookie_name
+  end
+
+  def refresh_token_cookie_name
+    TradeTariffIdentity.refresh_token_cookie_name
   end
 end
