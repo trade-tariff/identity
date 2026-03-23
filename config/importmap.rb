b067b0f76ebc0faf9e132163180e17b3c05a9089
Andrew Pepler <1190196+vertism@users.noreply.github.com>
2026-03-23 17:03:21 +0000

    Update govuk-frontend & govuk-components

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,4 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "govuk-frontend", to: "https://cdn.jsdelivr.net/npm/govuk-frontend@5.9.0"
+pin "govuk-frontend", to: "https://cdn.jsdelivr.net/npm/govuk-frontend@6.1.0/dist/govuk/govuk-frontend.min.js"
